a6e3d7a8174f4e619bd5c5e6c3a436418e3cbb17
crohnaro <viniciosalfa@hotmail.com>
2022-08-22 14:48:27 -0300

    Adicionando estrutura dinamica a classe para busca de character id

--- app/src/main/java/com/example/rickandmortyapi/RickandMortyService.kt
+++ app/src/main/java/com/example/rickandmortyapi/RickandMortyService.kt
@@ -2,9 +2,12 @@ package com.example.rickandmortyapi
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RickandMortyService {
 
-    @GET("character/2")
-    fun getCharacaterById(): Call<Any>
+    @GET("character/{character-id}")
+    fun getCharacaterById(
+        @Path("character-id") characterId: Int
+    ): Call<GenerateCharacterByIdResponse>
 }
