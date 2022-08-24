480deaee3310936cc74ea35dd22b47034eb7bc4c
crohnaro <viniciosalfa@hotmail.com>
2022-08-24 10:30:49 -0300

    Tratando exception request usando mvvm, criando um dataclass para isso

--- app/src/main/java/com/example/rickandmortyapi/ApiClient.kt
+++ app/src/main/java/com/example/rickandmortyapi/ApiClient.kt
@@ -5,7 +5,15 @@ import retrofit2.Response
 class ApiClient(
     private val rickandMortyService: RickandMortyService
 ) {
-    suspend fun getCharacterById(characterId: Int): Response<GenerateCharacterByIdResponse>{
-        return rickandMortyService.getCharacaterById(characterId)
+    suspend fun getCharacterById(characterId: Int): SimpleResponse<GenerateCharacterByIdResponse>{
+        return safeApiCall { rickandMortyService.getCharacaterById(characterId) }
+    }
+
+    private inline  fun <T> safeApiCall(apiCall: () -> Response<T>): SimpleResponse<T>{
+        return try {
+            SimpleResponse.success(apiCall.invoke())
+        } catch (e: Exception) {
+            SimpleResponse.failure(e)
+        }
     }
 }
