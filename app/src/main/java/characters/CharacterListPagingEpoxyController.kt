3e5e8ee89c8f2d73101d4c09bad34a3400ba5ee1
crohnaro <vinicioscararine95@gmail.com>
2022-09-12 22:59:29 -0300

    Adicionando tittles separando personagens por ordem alfabética

--- app/src/main/java/characters/CharacterListPagingEpoxyController.kt
+++ app/src/main/java/characters/CharacterListPagingEpoxyController.kt
@@ -5,8 +5,11 @@ import com.airbnb.epoxy.paging.PagedListEpoxyController
 import com.example.rickandmortyapi.GenerateCharacterByIdResponse
 import com.example.rickandmortyapi.R
 import com.example.rickandmortyapi.databinding.ModelCharacterListItemBinding
+import com.example.rickandmortyapi.databinding.ModelCharacterListTitleBinding
 import com.squareup.picasso.Picasso
+import epoxy.LoadingEpoxyModel
 import epoxy.ViewBindingKotlinModel
+import java.util.*
 
 class CharacterListPagingEpoxyController: PagedListEpoxyController<GenerateCharacterByIdResponse>() {
     override fun buildItemModel(
@@ -16,6 +19,30 @@ class CharacterListPagingEpoxyController: PagedListEpoxyController<GenerateChara
         return CharacterGridItemEpoxyModel(item!!.image, item.name).id(item.id)
     }
 
+    override fun addModels(models: List<EpoxyModel<*>>) {
+        if (models.isEmpty()){
+            LoadingEpoxyModel().id("loading").addTo(this)
+            return
+        }
+
+        CharacterGridTittleEpoxyModel("Main Family")
+            .id("main_family_header")
+            .addTo(this)
+
+        super.addModels(models.subList(0, 5))
+
+        (models.subList(5, models.size) as List<CharacterGridItemEpoxyModel> ).groupBy {
+            it.name[0].toUpperCase()
+        }.forEach { mapEntry ->
+            val character = mapEntry.key.toString().toUpperCase(Locale.US)
+            CharacterGridTittleEpoxyModel(title = character)
+                .id(character)
+                .addTo(this)
+
+            super.addModels(mapEntry.value)
+        }
+    }
+
     data class  CharacterGridItemEpoxyModel(
        val imageUrl: String,
        val name: String
@@ -23,6 +50,19 @@ class CharacterListPagingEpoxyController: PagedListEpoxyController<GenerateChara
         override fun ModelCharacterListItemBinding.bind() {
             Picasso.get().load(imageUrl).into(characterImageView)
             characterNameTextView.text = name
+        }
+    }
+
+    data class CharacterGridTittleEpoxyModel(
+        val title : String
+    ): ViewBindingKotlinModel<ModelCharacterListTitleBinding>(R.layout.model_character_list_title){
+
+        override fun ModelCharacterListTitleBinding.bind() {
+            textView.text = title
+        }
+
+        override fun getSpanSize(totalSpanCount: Int, position: Int, itemCount: Int): Int {
+            return totalSpanCount
         }
     }
 }
