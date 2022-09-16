4aa18927228b2d8c0fcb110ced004f4c5785c075
crohnaro <vinicioscararine95@gmail.com>
2022-09-16 11:14:38 -0300

    Adicionando debug network com chuck interceptor, para verificar as requisições

--- app/src/main/java/network/network/NetworkLayer.kt
+++ app/src/main/java/network/network/NetworkLayer.kt
@@ -1,13 +1,19 @@
 package network.network
 
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.dmp.simplemorty.SimpleMortyApplication
 import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object NetworkLayer {
     val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
     val retrofit = Retrofit.Builder()
+        .client(getLogginHttpClient())
         .baseUrl("https://rickandmortyapi.com/api/")
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
@@ -17,4 +23,22 @@ object NetworkLayer {
     }
 
     val apiClient = ApiClient(rickAndMortyService)
+
+    private fun getLogginHttpClient(): OkHttpClient{
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BASIC)
+        })
+
+        builder.addInterceptor(
+            ChuckerInterceptor.Builder(SimpleMortyApplication.context)
+                .collector(ChuckerCollector(SimpleMortyApplication.context))
+                .maxContentLength(250000L)
+                .redactHeaders(emptySet())
+                .alwaysReadResponseBody(false)
+                .build()
+        )
+
+        return builder.build()
+    }
 }
