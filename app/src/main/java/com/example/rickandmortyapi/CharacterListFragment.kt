fc131c7ccfce14dc79d85ff8862e8ec81fc7329c
crohnaro <viniciosalfa@hotmail.com>
2022-09-19 22:30:00 -0300

    migrando activities para fragments

--- app/src/main/java/com/example/rickandmortyapi/CharacterListFragment.kt
+++ app/src/main/java/com/example/rickandmortyapi/CharacterListFragment.kt
@@ -1,32 +1,24 @@
 package com.example.rickandmortyapi
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import characters.CharacterListPagingEpoxyController
+import characters.CharactersViewModel
+import com.airbnb.epoxy.EpoxyRecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CharacterListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CharacterListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val epoxyController = CharacterListPagingEpoxyController (::onCharacterSelected)
+
+    private val viewModel: CharactersViewModel by lazy {
+        ViewModelProvider(this)[CharactersViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -37,32 +29,23 @@ class CharacterListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_character_list, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view?.postDelayed({
-            val navGraphActivity = activity as NavGraphActivity
-            navGraphActivity.navController.navigate(R.id.action_characterListFragment_to_characterDetailFragment)
-        }, 1_500)
+        viewModel.charactersPagedListLiveData.observe(viewLifecycleOwner){ pagedList ->
+            epoxyController.submitList(pagedList)
+        }
+
+        view.findViewById<EpoxyRecyclerView>(R.id.epoxyRecycerView).setController(epoxyController)
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CharacterListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CharacterListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun onCharacterSelected(characterId: Int){
+       val directions =  CharacterListFragmentDirections.actionCharacterListFragmentToCharacterDetailFragment(
+            characterId = characterId
+        )
+        findNavController().navigate(directions)
     }
+
+
 }
