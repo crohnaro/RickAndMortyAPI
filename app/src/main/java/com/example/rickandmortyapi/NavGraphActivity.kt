ca8a3d1fa950e0e042302d0be76f06b2063cee40
crohnaro <crohnarodev@gmail.com>
2022-09-23 07:23:16 -0300

    adicionando drawer layout

--- app/src/main/java/com/example/rickandmortyapi/NavGraphActivity.kt
+++ app/src/main/java/com/example/rickandmortyapi/NavGraphActivity.kt
@@ -2,12 +2,15 @@ package com.example.rickandmortyapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NavGraphActivity: AppCompatActivity() {
 
@@ -20,13 +23,18 @@ class NavGraphActivity: AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
         setupActionBarWithNavController(
             navController = navController,
             configuration = appBarConfiguration
         )
+
+
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
