a4c95411de7751563d18e0270ef57a9723ce848f
crohnaro <vinicioscararine95@gmail.com>
2022-11-13 19:22:31 -0300

    ending of the project! that take too long, dude!

--- app/src/main/java/characters/search/CharacterSearchEpoxyController.kt
+++ app/src/main/java/characters/search/CharacterSearchEpoxyController.kt
@@ -5,6 +5,7 @@ import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging3.PagingDataEpoxyController
 import com.example.rickandmortyapi.R
 import com.example.rickandmortyapi.databinding.ModelCharacterListItemBinding
+import com.example.rickandmortyapi.databinding.ModelLocalExceptionErrorStateBinding
 import com.squareup.picasso.Picasso
 import domain.models.Character
 import epoxy.LoadingEpoxyModel
@@ -15,6 +16,15 @@ import kotlinx.coroutines.ObsoleteCoroutinesApi
 class CharacterSearchEpoxyController(
     private val onCharacterSelected: (Int) -> Unit
 ): PagingDataEpoxyController<domain.models.Character>() {
+
+    var localException: CharacterSearchPagingSource.LocalException? = null
+        set(value){
+            field = value
+            if (localException != null){
+                requestModelBuild()
+            }
+        }
+
     override fun buildItemModel(currentPosition: Int, item: Character?): EpoxyModel<*> {
         return CharacterGridItemEpoxyModel(
             characterId = item!!.id,
@@ -27,6 +37,12 @@ class CharacterSearchEpoxyController(
     }
 
     override fun addModels(models: List<EpoxyModel<*>>) {
+
+        if (localException != null){
+            LocalExceptionErrorStateEpoxyModel(localException!!).id("error_state").addTo(this)
+            return
+        }
+
         if (models.isEmpty()) {
             LoadingEpoxyModel().id("loading").addTo(this)
             return
@@ -48,6 +64,19 @@ class CharacterSearchEpoxyController(
             root.setOnClickListener{
                 onCharacterSelected(characterId)
             }
+        }
+    }
+
+    data class LocalExceptionErrorStateEpoxyModel(
+        val localException: CharacterSearchPagingSource.LocalException
+    ): ViewBindingKotlinModel<ModelLocalExceptionErrorStateBinding>(R.layout.model_local_exception_error_state) {
+        override fun ModelLocalExceptionErrorStateBinding.bind(){
+            titleTextView.text = localException.tittle
+            descriptionTextView.text = localException.description
+        }
+
+        override fun getSpanSize(totalSpanCount: Int, position: Int, itemCount: Int): Int {
+            return totalSpanCount
         }
     }
 }
